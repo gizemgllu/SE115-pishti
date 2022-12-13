790340d29274271b9a9aea7bed5304056297afa6
gizemgllu <120049270+gizemgllu@users.noreply.github.com>
2022-12-14 00:51:40 +0300

    added cards score method

--- Structure.java
+++ Structure.java
@@ -2,6 +2,8 @@ public class Structure {
     private String numbers;
     private String letters;
 
+    private int cardScore;
+
     public Structure(String numbers, String letters) {
         this.numbers= numbers;
         this.letters=letters;
@@ -11,6 +13,14 @@ public class Structure {
     }
     public String getLetters() {
         return this.letters;
+    }
+
+    public int getCardScore() {
+        return this.cardScore;
+    }
+    public void setCardScore(int cardScore) {
+
+        this.cardScore=cardScore;
     }
 
     public String toString() {
