fcb7806ebcb8b63117b4b236444a2f43bc91c5c5
gizemgllu <120049270+gizemgllu@users.noreply.github.com>
2022-12-15 00:13:18 +0300

    added cut method

--- Structure.java
+++ Structure.java
@@ -1,19 +1,35 @@
 public class Structure {
     private String numbers;
     private String letters;
+    private int cardsScore;
 
     public Structure(String numbers, String letters) {
         this.numbers= numbers;
         this.letters=letters;
     }
+    public Structure(int cardsScore) {
+        this.cardsScore=cardsScore;
+    }
     public String getNumbers() {
+
         return this.numbers;
     }
     public String getLetters() {
+
         return this.letters;
+    }
+    public int getCardsScore() {
+
+        return this.cardsScore;
+    }
+
+    public void setLetters(int cardsScore) {
+
+        this.cardsScore = cardsScore;
     }
 
     public String toString() {
+
         return letters+ "-" +numbers;
     }
 
