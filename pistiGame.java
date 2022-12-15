f0267837d5e813fb694656f4f0526eaa18b4f91f
gizemgllu <120049270+gizemgllu@users.noreply.github.com>
2022-12-15 15:19:24 +0300

    added cut method

--- pistiGame.java
+++ pistiGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class pistiGame {
 
@@ -10,10 +11,10 @@ public class pistiGame {
     private String[] cardsScore;
 
     public pistiGame() {
-        cardsUsed=0;
+        cardsUsed = 0;
         currentC = 0;
         String[] letters = {"C", "D", "H", "S"};
-        String[] numbers = {"A","2", "3", "4", "5", "6", "7", "8", "9", "10","J", "K", "Q"};
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"};
         deckOfCards = new Structure[numbCards];
 
         for (int i = 0; i < deckOfCards.length; i++) {
@@ -21,50 +22,84 @@ public class pistiGame {
 
         }
     }
+
+    public void setDeckOfCards() {
+        this.deckOfCards = deckOfCards;
+    }
+
     public int getNumbCards() {
+
         return this.numbCards;
     }
-    public void shuffleCards() {
 
-        for (int i =deckOfCards.length;i>0; i--) {
-            int rndNumbers=(int)(Math.random()*(i+1));
-            Structure tmp = deckOfCards[i];
-            deckOfCards[i]=deckOfCards[rndNumbers];
-            deckOfCards[rndNumbers]=tmp;
-        }
-             cardsUsed=  0;
-    }
+
     public Structure giveCard(int numbCards) {
         numbCards--;
         cardsUsed++;
         System.out.println("Number of cards left: " + cardsLeft());
         return deckOfCards[numbCards];
     }
+
     public Structure showCard(int numbCards) {
 
         return deckOfCards[numbCards];
     }
-    public int cardsLeft(){
 
-        return deckOfCards.length-cardsUsed;
+    public int cardsLeft() {
+
+        return deckOfCards.length - cardsUsed;
     }
+
     public Structure dealingCards() {
-        if(currentC< deckOfCards.length) {
+        if (currentC < deckOfCards.length) {
             return deckOfCards[cardsUsed++];
-        }
-        else{
+        } else {
             return null;
         }
-        public void cut(){
-            Random r= new Random();
-            int cutPosition=r.nextInt(numbCards);
-            String[] newCards= new String[numbCards];
-            for(int i=0; i<numbCards;i++) {
-                newCards[i]=cardsScore[(cutPosition+i)%numbCards];
-            }
+    }
 
-
+    public void cut() {
+        Random r = new Random();
+        int cutPosition = r.nextInt(numbCards);
+        String[] newCards = new String[numbCards];
+        for (int i = 0; i < numbCards; i++) {
+            newCards[i] = cardsScore[(cutPosition + i) % numbCards];
         }
     }
 
+    public void shuflleCards() {
+
+        for (int i = deckOfCards.length - 1; i > 0; i--) {
+            int rndNumbers = (int) (Math.random() * (i + 1));
+            Structure tmp = deckOfCards[i];
+            deckOfCards[i] = deckOfCards[rndNumbers];
+            deckOfCards[rndNumbers] = tmp;
+            System.out.println(deckOfCards[rndNumbers]);
+        }
+        cardsUsed = 0;
+    }
+
+    public static void cut(String [] deck) {
+        System.out.println("Who cut the deck user 1,computer 2 ");
+        Scanner sc = new Scanner(System.in);
+        int answer = sc.nextInt();
+        if (answer == 1) {
+            System.out.println("pick number");
+            int a = sc.nextInt();
+            String[] deck2 = new String[52];
+            String[] deck3 = new String[52];
+            System.arraycopy(deck, 0, deck2, 0, a);
+            System.arraycopy(deck, 0, deck3, 52 - a, 52);
+
+        }
+        if(answer==2) {
+            Random r= new Random();
+            int comp=r.nextInt(52);
+            String[] deck2 = new String[52];
+            String[] deck3 = new String[52];
+            System.arraycopy(deck, 0, deck2, 0, comp);
+            System.arraycopy(deck, 0, deck3, 52 - comp, 52);
+
+        }
+    }
 }
