3d0f6f78c70d98bde2bc9f36a83d39ce29a7c046
gizemgllu <120049270+gizemgllu@users.noreply.github.com>
2022-12-25 23:38:57 +0300

    added user's and computer's playing conditions

--- pistiGame.java
+++ pistiGame.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class pistiGame {
     private final String[] suits = {"♥", "♦", "♠", "♣" };
@@ -105,8 +106,93 @@ public class pistiGame {
         cardsUsed += 4;
         boardSize = 4;
     }
+    public void playGame() {
+        shuffleDeck();
+        cut(); // cut the deck before dealing the cards
 
+        dealBoard(); // deal 4 cards to the board
+
+        for (int i = 0; i < 6; i++) {
+            dealCards(); // deal 4 cards to each player
+            for (int j = 0; j < 4; j++) {
+                printUserHand();
+                // display the last card on the board to the players
+                if (boardSize != 0) {
+                    System.out.println("The last card on the board is: " + board[boardSize - 1]);
+                } else {
+                    System.out.println("Board is empty");
+                }
+                userCardPlay();
+                computerCardPlay();
+            }
+        }
+        gameFinished();
+    }
+    public void userCardPlay() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select a card index to play:");
+        int selectedCard = sc.nextInt();
+        System.out.println("You played " + userHand[selectedCard]);
+        playCard(userHand[selectedCard], true);  // ı will create playCard func to play all game
+        for (int i = selectedCard; i < userHandSize - 1; i++) {
+            userHand[i] = userHand[i + 1];
+        }
+        userHand[userHandSize - 1] = null;
+        userHandSize--;
+    }
+
+    public void computerCardPlay() {
+        boolean played = false;
+        int selectedCard = 0;
+        if (boardSize == 0) {
+            selectedCard = 0;
+            played = true;
+        }
+        if (played == false) {
+            for (int i = 0; i < computerHandSize; i++) {
+                if (computerHand[i].getRanks().equals(board[boardSize - 1].getRanks())) {
+                    selectedCard = i;
+                    played = true;
+                }
+            }
+        }
+        else if (played == false) {
+            for (int i = 0; i < computerHandSize; i++) {
+                if (computerHand[i].getRanks().equals("J")) {
+                    selectedCard = i;
+                    played = true;
+                }
+            }
+        }
+        else if (played == false) {
+            selectedCard = 0;
+        }
+        System.out.println("Computer played " + computerHand[selectedCard]);
+        playCard(computerHand[selectedCard], false); // will create a playCard func to play the game
+        for (int i = selectedCard; i < computerHandSize - 1; i++) {
+            computerHand[i] = computerHand[i + 1];
+        }
+        computerHand[computerHandSize - 1] = null;
+        computerHandSize--;
+    }
+
+
+
+
+
+
+
+
+
+
+    // Print the user's hand
     public void printUserHand() {
+        System.out.println("Your hand: ");
+        for (int i = 0; i < userHand.length; i++) {
+            if (userHand[i] != null)
+                System.out.println(i + ": " + userHand[i]);
+        }
+
         System.out.println("Your hand: ");
         for (Structure card : this.userHand) {
             System.out.println(card);
@@ -131,6 +217,102 @@ public class pistiGame {
         System.out.println("Thanks for playing.");
         System.out.println("-------------------------");
     }
+    public int getCardsUsed() {
+        return cardsUsed;
+    }
+
+    public void setCardsUsed(int cardsUsed) {
+        this.cardsUsed = cardsUsed;
+    }
+
+    public Structure[] getDeckOfCards() {
+        return deckOfCards;
+    }
+
+    public void setDeckOfCards(Structure[] deckOfCards) {
+        this.deckOfCards = deckOfCards;
+    }
+
+    public Structure[] getUserHand() {
+        return userHand;
+    }
+
+    public void setUserHand(Structure[] userHand) {
+        this.userHand = userHand;
+    }
+
+    public Structure[] getComputerHand() {
+        return computerHand;
+    }
+
+    public void setComputerHand(Structure[] computerHand) {
+        this.computerHand = computerHand;
+    }
+
+    public Structure[] getBoard() {
+        return board;
+    }
+
+    public void setBoard(Structure[] board) {
+        this.board = board;
+    }
+
+    public Structure[] getUserTaken() {
+        return userTaken;
+    }
+
+    public void setUserTaken(Structure[] userTaken) {
+        this.userTaken = userTaken;
+    }
+
+    public Structure[] getComputerTaken() {
+        return computerTaken;
+    }
+
+    public void setComputerTaken(Structure[] computerTaken) {
+        this.computerTaken = computerTaken;
+    }
+
+    public int getUserTakenSize() {
+        return userTakenSize;
+    }
+
+    public void setUserTakenSize(int userTakenSize) {
+        this.userTakenSize = userTakenSize;
+    }
+
+    public int getComputerTakenSize() {
+        return computerTakenSize;
+    }
+
+    public void setComputerTakenSize(int computerTakenSize) {
+        this.computerTakenSize = computerTakenSize;
+    }
+
+    public int getBoardSize() {
+        return boardSize;
+    }
+
+    public void setBoardSize(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+    public int getUserHandSize() {
+        return userHandSize;
+    }
+
+    public void setUserHandSize(int userHandSize) {
+        this.userHandSize = userHandSize;
+    }
+
+    public int getComputerHandSize() {
+        return computerHandSize;
+    }
+
+    public void setComputerHandSize(int computerHandSize) {
+        this.computerHandSize = computerHandSize;
+    }
+
     public int getUserScore() {
         return userScore;
     }
@@ -146,6 +328,10 @@ public class pistiGame {
     public void setComputerScore(int computerScore) {
         this.computerScore = computerScore;
     }
+
+}
+
+
 
 }
 
